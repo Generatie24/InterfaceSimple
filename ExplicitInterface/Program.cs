b007e82590cbe2a110d75b2ea7ced2d58a44d228
kenan kurda <Intecbrussel2122@gmail.com>
2023-12-03 15:08:51 +0100

    Added interface explicit

---
+++ ExplicitInterface/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterface
+{
+
+    interface IInterfaceA
+    {
+        void DoeIets();
+    }
+
+    interface IInterfaceB
+    {
+        void DoeIets();
+    }
+
+    public class DemoKlasse : IInterfaceA, IInterfaceB
+    {
+        void IInterfaceA.DoeIets()
+        {
+            Console.WriteLine("DoeIets methode aangeroepen via IInterfaceA");
+        }
+
+        void IInterfaceB.DoeIets()
+        {
+            Console.WriteLine("DoeIets methode aangeroepen via IInterfaceB");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoKlasse demo = new DemoKlasse();
+
+            // Aanroepen via IInterfaceA
+            IInterfaceA a = demo;
+            a.DoeIets();
+
+            // Aanroepen via IInterfaceB
+            IInterfaceB b = demo;
+            b.DoeIets();
+        }
+    }
+}
